518e8fe12189172552061107b2f4e98e40929dbe
PedroJCAlmeida <pedrojoaocarvalhoalmeida@gmail.com>
2025-01-22 14:04:27 +0000

    Making components reasuble

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data';
+
 const reactDescription = ['Fundamental', 'Crucial', 'Core']
 
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max+1));
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
 function Header() {
   const description = reactDescription[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
@@ -18,11 +21,35 @@ function Header() {
   );
 }
 
+function CoreConcepts({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcepts {...CORE_CONCEPTS[1]}/>
+            <CoreConcepts {...CORE_CONCEPTS[2]}/>
+            <CoreConcepts {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
